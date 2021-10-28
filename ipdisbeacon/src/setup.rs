87dd7523be16fc51cc49b47197ab3e3b88eaaf80
Sandro Pratesi <sandro.pratesi@gmx.com>
2021-10-28 13:02:13 +0200

    beacon: minimal viable implementation

---
+++ ipdisbeacon/src/setup.rs
@@ -0,0 +1,16 @@
+use color_eyre::Report;
+use tracing_subscriber::EnvFilter;
+
+pub fn setup() -> Result<(), Report> {
+    if std::env::var("RUST_BACKTRACE").is_err() {
+        std::env::set_var("RUST_BACKTRACE", "1")
+    }
+    color_eyre::install()?;
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "warn")
+    }
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+    Ok(())
+}
